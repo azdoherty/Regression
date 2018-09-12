ef6fde249e429fe08e69535ada7d1e71948a45b4
adoherty8 <azdoherty91@gmail.com>
2018-09-11 20:28:33 -0400

    add LOO validation to linear regression

--- regression.py
+++ regression.py
@@ -1,9 +1,13 @@
 import numpy as np
-
+import itertools
 
 def objective_function(X, m, b):
     print("merge")
     res = np.sum((X[:, 1] - (m*X[:,0] - b))**2)
     print(np.sqrt(res))
     return res
+
+r = itertools.combinations([1,2,3,4,5], 2)
+for i in r:
+    print(i)
 
